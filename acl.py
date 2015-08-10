3b2896d4447eac39453cf50f613be3df4b9b6f58
Greg Bowser <topnotcher@gmail.com>
2015-08-09 22:22:13 -0400

    initial

---
+++ acl.py
@@ -0,0 +1,393 @@
+import inspect
+import re
+
+class ACL(object):
+    def __init__(self, name):
+        self.rules = []
+
+    def add_ace(self, ace):
+        self.rules.append(ace)
+
+    def __str__(self):
+        st = 'Access list: %s\n'
+
+        for rule in self.rules:
+            st += str(rule) + '\n'
+
+        return st
+
+class ACE(object):
+    def __init__(self, entry, remarks=None):
+        self.entry = entry
+        self.rules = []
+        self.remarks = remarks
+
+    def add_rule(self, rule):
+        self.rules.append(rule)
+
+    def __str__(self):
+        st = '\n'.join(self.remarks) + '\n' + '-' * 100 + '\n'
+        st += self._str_rule(1, self.entry)
+        st +=  '-' * 100 + '\n'
+        for rule in self.rules:
+            st += self._str_rule(2, rule)
+
+        return st
+
+    @staticmethod
+    def _str_rule(level, rule):
+        st = ' ' * (4*level) + rule['_raw'] + '\n'
+        for field in rule:
+            if field not in ('_raw', 'acl_name'):
+                st +=  ' '*(4*(level+1)) + '%s: %s\n' % (field, str(rule[field]))
+
+        return st
+
+def get_indent(line):
+    i = 0
+    for ch in line:
+        if ch != ' ':
+            break
+        else:
+            i += 1
+    return i
+
+def parse_hdr(line):
+    pcs = line.split('; ')
+
+    word, name = pcs[0].split(' ')
+    assert word == 'access-list'
+
+    elements, word = pcs[1].split(' ')
+    assert word == 'elements'
+
+    return name, int(elements)
+
+def is_ip_addr(ip):
+    octets = [int(octet) for octet in ip.split('.', 3) if octet.isdigit()]
+    return len(octets) == 4 and min(octets) >= 0 and max(octets) <= 255
+
+class ACELineParser(object):
+    def __init__(self, line):
+        self.line = line
+        self.toks = []
+        self.data = {}
+        # self.data.keys(), but ordered
+        self.populated = []
+
+    def populate(self, key, val):
+        self.data[key] = val
+        self.populated.append(key)
+
+    def parse(self):
+        line = self.line.strip()
+        self.data['_raw'] = line
+        self.toks = line.split()
+
+        while len(self.toks) > 0:
+            self.consume_toks()
+
+        return self.data
+
+    def consume_toks(self):
+        methods = inspect.getmembers(self, self.isinstancemethod)
+        consumers = (meth[1] for meth in methods if meth[0].startswith('_consume'))
+
+        start_len = len(self.toks)
+        for meth in consumers:
+            if len(self.toks):
+                meth()
+            else:
+                return
+
+        if len(self.toks) == start_len:
+            raise ValueError('Unable to consume all toks: (%s)\n%s' % (', '.join(self.toks), str(self.data)))
+
+    def isinstancemethod(self, meth):
+        if not inspect.ismethod(meth):
+            return False
+        elif type(meth.__self__) != type(self):
+            return False
+
+        return True
+
+    def require_next_tok(*args):
+        """
+        Require that the next token be in *args
+        """
+        def decorate(meth):
+            def validate(ins):
+                if ins.toks[0] in args:
+                    meth(ins)
+            return validate
+
+        return decorate
+
+
+    def consume_next_tok(*args):
+        """
+        same as require_next tok, but consume it.
+        """
+        def decorate(meth):
+            def validate(ins):
+                if ins.toks[0] in args:
+                    ins.toks.pop(0)
+                    meth(ins)
+            return validate
+
+        return decorate
+
+    def require_not_populated(*args):
+        """
+        Require that not all of the arguments have been parsed
+        """
+        def decorate(meth):
+            def validate(ins):
+                if set(args) - set(ins.populated):
+                    meth(ins)
+            return validate
+
+        return decorate
+
+    def require_populated(*args):
+        """
+        Require that all of the arguments have been parsed
+        """
+        def decorate(meth):
+            def validate(ins):
+                if not (set(args) - set(ins.populated)):
+                    meth(ins)
+            return validate
+
+        return decorate
+
+    def require_prev_field(*args):
+        def decorate(meth):
+            def validate(ins):
+                if ins.populated[-1] in args:
+                    meth(ins)
+            return validate
+
+        return decorate
+
+    @consume_next_tok('access-list')
+    @require_not_populated('acl_name')
+    def _consume_acl_name(self):
+        self.populate('acl_name', self.toks.pop(0))
+
+    @consume_next_tok('line')
+    @require_not_populated('line')
+    def _consume_line_number(self):
+        self.populate('line', int(self.toks.pop(0)))
+
+    @require_next_tok('extended', 'remark')
+    @require_not_populated('type')
+    def _consume_type(self):
+        self.populate('type', self.toks.pop(0))
+        if self.data['type'] == 'remark':
+            self.populate('remark', ' '.join(self.toks))
+            self.toks = []
+
+    @require_next_tok('permit', 'deny')
+    @require_not_populated('action')
+    def _consume_action(self):
+        self.populate('action', self.toks.pop(0))
+
+    @require_not_populated('ip_proto')
+    @require_prev_field('action')
+    def _consume_ip_proto(self):
+        next_tok = self.toks.pop(0)
+
+        if next_tok in ('object-group', 'object'):
+            proto = {next_tok: self.toks.pop(0)}
+        else:
+            proto = next_tok
+
+        self.populate('ip_proto', proto)
+
+    @require_prev_field('ip_proto')
+    def _consume_src_dst(self):
+        """
+        This is a stupid and ugly way of parsing source/source port/dst/dest port.
+
+        Objects or object groups could look like a source port or a
+        destination. It's easy to tell if you have the object definition handy,
+        but based on the ACE alone is more difficult: if the thing after the
+        source parses correctly as a service or a destination, try parsing
+        further to see if there is a valid destination.
+        """
+        self.populate('src', self._try_consume_src_dst(self.toks))
+
+        test_dst_toks = self.toks[:]
+        test_sport_toks = self.toks[:]
+        dst = self._try_consume_src_dst(test_dst_toks)
+        sport = self._try_consume_sport_dport(test_sport_toks)
+
+        # could be either (object or object-group)
+        if sport and dst:
+            # if it's an sport, it needs to be followed by a dst
+            if self._try_consume_src_dst(test_sport_toks):
+                self.populate('src_service', self._try_consume_sport_dport(self.toks))
+                self.populate('dst', self._try_consume_src_dst(self.toks))
+
+                if self._try_consume_sport_dport(self.toks[:]):
+                    self.populate('dst_service', self._try_consume_sport_dport(self.toks))
+
+            # if it's a dst, it needs to be followed by a dport, or nothing
+            else:
+                self.populate('dst', self._try_consume_src_dst(self.toks))
+
+                #print(', '.join(self.toks))
+                #print(len(self.toks))
+                if self._try_consume_sport_dport(test_dst_toks):
+                    self.populate('dst_service', self._try_consume_sport_dport(self.toks))
+
+        # next thing could be a src/dst or source port.
+        elif sport:
+            self.populate('src_service', self._try_consume_sport_dport(self.toks))
+            self.populate('dst', self._try_consume_src_dst(self.toks))
+
+            if self._try_consume_sport_dport(self.toks[:]):
+                self.populate('dst_service', self._try_consume_sport_dport(self.toks))
+
+        elif dst:
+            self.populate('dst', self._try_consume_src_dst(self.toks))
+
+            if self._try_consume_sport_dport(self.toks[:]):
+                self.populate('dst_service', self._try_consume_sport_dport(self.toks))
+
+    def _try_consume_src_dst(self, toks):
+        if not len(toks):
+            return None
+
+        next_tok = toks[0]
+        next_next_tok = toks[1] if len(toks) > 1 else None
+        target = {}
+
+        if next_tok == 'host':
+            toks.pop(0)
+            target['host'] = toks.pop(0)
+
+        elif next_tok == 'range' and is_ip_addr(next_next_tok):
+            toks.pop(0)
+            target['range'] = (toks.pop(0), toks.pop(0))
+
+        elif is_ip_addr(next_tok) and is_ip_addr(next_next_tok):
+            target['net'] = (toks.pop(0), toks.pop(0))
+
+        elif next_tok in ('object-group', 'object'):
+            toks.pop(0)
+            target[next_tok] = toks.pop(0)
+
+        elif next_tok == 'any':
+            toks.pop(0)
+            target['any'] = True
+
+        return target
+
+    #@require_prev_field('src', 'dst')
+    def _try_consume_sport_dport(self, toks):
+        if not len(toks):
+            return None
+
+        next_tok = toks[0]
+        next_next_tok = toks[1] if len(toks) > 1 else None
+        target = {}
+
+        if next_tok in ('eq', 'neq', 'gt', 'lt'):
+            qual = toks.pop(0)
+            target[qual] = toks.pop(0)
+
+        elif next_tok == 'range' and next_next_tok.isdigit():
+            toks.pop(0)
+            target['range'] = (toks.pop(0), toks.pop(0))
+
+        elif self.data['ip_proto'] == 'icmp' and next_tok in ('echo', 'time-exceeded', 'unreachable'):
+            target['icmp_type'] = toks.pop(0)
+
+        elif next_tok in ('object-group', 'object'):
+            toks.pop(0)
+            target[next_tok] = toks.pop(0)
+
+        return target
+
+    @consume_next_tok('log')
+    def _consume_log(self):
+        next_tok = self.toks[0]
+
+        if next_tok in ('emergencies', 'informational', 'disable', 'errors', 'alerts', 'debugging') or next_tok.isdigit():
+            self.toks.pop(0)
+
+        if next_tok == 'interval':
+            self.toks.pop(0)
+            self.toks.pop(0)
+
+    @consume_next_tok('inactive')
+    def _consume_disable(self):
+        self.populate('inactive', True)
+
+    def _consume_hitcnt(self):
+        next_tok = self.toks[0]
+        if re.match('\(hitcnt=[0-9]+\)', next_tok):
+            self.toks.pop(0) # hitcnt
+            self.toks.pop(0) #hash
+
+
+class ACLParser(object):
+    def __init__(self, name, elements):
+        self.elements = elements
+        self.consumed_elements = 0
+        self.remarks = []
+
+        self.acl = ACL(name)
+        self.ace = None
+
+    def consume(self, it):
+        while self.consumed_elements < self.elements:
+            try:
+                self.consume_line(next(it))
+            except StopIteration:
+                break
+
+        return self.acl
+
+    def consume_line(self, line):
+        level = get_indent(line)
+
+        parser = ACELineParser(line)
+        data = {}
+        data = parser.parse()
+
+        if data['type'] == 'extended':
+            self.consumed_elements += 1
+
+        if level == 0:
+            if data['type'] == 'remark':
+                self.remarks.append(data['remark'])
+            else:
+                if self.ace is not None:
+                    self.acl.add_ace(self.ace)
+
+                self.ace = ACE(data, self.remarks)
+                self.remarks = []
+
+        elif level == 2:
+            self.ace.add_rule(data)
+
+def parse_file(file_name):
+    fh = open(file_name, 'r')
+
+    acls = []
+    for line in fh:
+        name, acl_elements = parse_hdr(line)
+        parser = ACLParser(name, acl_elements)
+        acl = parser.consume(fh)
+        acls.append(acl)
+
+    fh.close()
+    print('%d acls' % len(acls))
+    for acl in acls:
+        print(acl)
+
+if __name__ == '__main__':
+    parse_file('data/acl.txt')
